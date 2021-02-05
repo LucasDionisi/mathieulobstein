02b50d921f8cb1b9310f0ad8ab1314b0ddce636f
Lucas Dionisi <lucas.dionisi@gmail.com>
2021-02-05 09:50:02 +0100

    Adds magnific-popup

--- com/js/realisation.js
+++ com/js/realisation.js
@@ -55,3 +55,11 @@ function setPhotos() {
 }
 
 setPhotos();
+
+
+$(document).ready(function() {
+    $('#headerVideoLink').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
