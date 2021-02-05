3d6395c5adadb483338eaf9850a8323f2c387fb2
Lucas Dionisi <lucas.dionisi@gmail.com>
2021-02-05 17:43:16 +0100

    Update js home

--- com/js/home.js
+++ com/js/home.js
@@ -1,4 +1,5 @@
 const homeTitles = ["Mathieu LOBSTEIN", "Production Audiovisuelle", "Drone", "Publicité", "Clip", "Fiction"];
+const homeTitles_en = ["Mathieu LOBSTEIN", "Audiovisual Production", "Drone", "Ads", "Clip", "Fiction"];
 const timeoutTime = 5000;
 const animationText = 750;
 let index = 1;
@@ -10,7 +11,8 @@ setTimeout(function changeHomeTitle() {
 }, timeoutTime);
 
 function setTitle() {
-    $('#homeTitle')[0].innerHTML = homeTitles[index];
+    if (!window.location.pathname.includes("index_en")) $('#homeTitle')[0].innerHTML = homeTitles[index];
+    else $('#homeTitle')[0].innerHTML = homeTitles_en[index];
     $('#homeTitle').fadeIn(animationText);
     index = (index + 1) % homeTitles.length;
 }
