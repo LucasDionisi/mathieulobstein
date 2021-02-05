572de065d19812b35be4d28638f5838ecf4b5448
Lucas Dionisi <lucas.dionisi@gmail.com>
2021-02-05 17:55:40 +0100

    Updates realization

--- com/js/realisation-1.1.js
+++ com/js/realisation-1.1.js
@@ -5,18 +5,18 @@ const FICTION = pathImg+"Fiction/";
 const PUBLICITE = pathImg+"Les_Publictes/";
 const YOUTUBE_LINK = "https://www.youtube.com/watch?v=";
 
-const clips = [{"path": CLIPS+"Clip_1_Danse_Julie.jpg", "name": "Constellation - Julie", "url": YOUTUBE_LINK +"vPje0HRA0h0", "filter": "clip"},
-			   {"path" :CLIPS+"Clip_2_HeaderSet1.png", "name": "DJset HEADER", "url": YOUTUBE_LINK +"fCPz8fTq3ag", "filter": "clip"}];
+const clips = [{"path": CLIPS+"Clip_1_Danse_Julie.jpg", "name": "Constellation - Julie", "name_en": "Constellation - Julie", "url": YOUTUBE_LINK +"vPje0HRA0h0", "filter": "clip"},
+			   {"path" :CLIPS+"Clip_2_HeaderSet1.png", "name": "DJset HEADER", "name_en": "DJset HEADER", "url": YOUTUBE_LINK +"fCPz8fTq3ag", "filter": "clip"}];
 
-const drone = [{"path": DRONE+"Drone_1_Alpes.jpg", "name": "Les Alpes", "url": "#", "filter": "drone"}, 
-               {"path": DRONE+"Drone_2_Revest.jpg", "name": "Le Revest", "url": "#", "filter": "drone"},
-               {"path": DRONE+"Drone_3_Ollioule.jpg", "name": "Ollioule", "url": YOUTUBE_LINK +"jHCbtdzp0vE", "filter": "drone"}]; 
+const drone = [{"path": DRONE+"Drone_1_Alpes.jpg", "name": "Les Alpes", "name_en": "The Alps", "url": "#", "filter": "drone"}, 
+               {"path": DRONE+"Drone_2_Revest.jpg", "name": "Le Revest", "name_en": "The Revest", "url": "#", "filter": "drone"},
+               {"path": DRONE+"Drone_3_Ollioule.jpg", "name": "Ollioules", "name_en": "Ollioules", "url": YOUTUBE_LINK +"jHCbtdzp0vE", "filter": "drone"}]; 
 
-const fiction = [{"path": FICTION+"Fiction_1_The_Shelter.jpg", "name": "The Shelter", "url": YOUTUBE_LINK +"0fIIciHKRlY", "filter": "fiction"},
-                 {"path": FICTION+"Fiction_2_Le_Coffre.jpg", "name": "Le Coffre", "url": YOUTUBE_LINK +"Xs8_PofLvUQ", "filter": "fiction"},
-                 {"path": FICTION+"Fiction_3_Hunting.jpg", "name": "Hunting", "url": YOUTUBE_LINK +"qhbjP1m5xiI", "filter": "fiction"}];
+const fiction = [{"path": FICTION+"Fiction_1_The_Shelter.jpg", "name": "The Shelter", "name_en": "The Shelter", "url": YOUTUBE_LINK +"0fIIciHKRlY", "filter": "fiction"},
+                 {"path": FICTION+"Fiction_2_Le_Coffre.jpg", "name": "Le Coffre", "name_en": "The Chest", "url": YOUTUBE_LINK +"Xs8_PofLvUQ", "filter": "fiction"},
+                 {"path": FICTION+"Fiction_3_Hunting.jpg", "name": "Hunting", "name_en": "Hunting", "url": YOUTUBE_LINK +"qhbjP1m5xiI", "filter": "fiction"}];
 
-const pub = [{"path": PUBLICITE+"Pub_1_Teaser_Mariage_Sam_Pierrick.jpg", "name": "Mariage Sam & Pierrick", "url": YOUTUBE_LINK +"w4O8j_Y2Glo", "filter": "pub"}];
+const pub = [{"path": PUBLICITE+"Pub_1_Teaser_Mariage_Sam_Pierrick.jpg", "name": "Mariage Sam & Pierrick", "name_en": "Sam & Pierrick wedding", "url": YOUTUBE_LINK +"w4O8j_Y2Glo", "filter": "pub"}];
 
 
 function setPhotos() {
@@ -40,13 +40,15 @@ function setPhotos() {
         photos.push(pub[i]);
     }
 
+    let key_name = window.location.pathname.includes("realisation_en") ? "name_en" : "name";
+
     for (let i = 0; i < photos.length; i++) {
         content += "<div class=\"imgNetB portfolioBox " +photos[i]["filter"]  +"\">";            
             content += "<div class=\"portfolioBox_in\" data-image=\"" +photos[i]["path"] +"\"></div>";
             content += "<div class=\"portfolioBox_info\">";
                 let lien = `<a class="popup-youtube portfolioBox_btn" href="${photos[i]["url"]}"><i class="fa fa-play"></i></a>`;
                 if (photos[i]["url"] == "" || photos[i]["url"] == "#") lien = "";
-                content += "<div class=\"portfolioBox_title\">" +photos[i]["name"] +lien +"</div>";
+                content += "<div class=\"portfolioBox_title\">" +photos[i][key_name] +lien +"</div>";
             content += "</div>";     
         content += "</div>";
     }
